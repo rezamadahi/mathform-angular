2ef8c2cd37999c0cab746b03b2be6a5515298562
unknown <rezamaddahi72@gmail.com>
2022-11-12 02:56:55 +0330

    add custom validators

---
+++ src/app/equation/equation.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-equation',
+  templateUrl: './equation.component.html',
+  styleUrls: ['./equation.component.css']
+})
+export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl(''),
+  }, [
+    (form: AbstractControl) => {
+    const { a, b, answer } = form.value;
+      if (a + b === parseInt(answer)) {
+        return null;
+      } else {
+        return { addition: true };
+      }
+    }
+  ]);
+
+  constructor() { }
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit(): void {
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10 );
+  }
+
+}
